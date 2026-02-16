63cdea9b528389c0fa3fc5c0493c1cd5f26f55a2
Hana Skalova <haskalov@c3r2s2.42prague.com>
2026-02-16 22:32:17 +0100

    ok

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: haskalov <haskalov@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/10 13:37:42 by haskalov          #+#    #+#             */
-/*   Updated: 2026/02/16 22:06:59 by haskalov         ###   ########.fr       */
+/*   Updated: 2026/02/16 22:32:03 by haskalov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,8 +27,6 @@ void free_map(char **map)
 
 int main(int argc, char *argv[])
 {
-	// int fd;
-	// char *line;
 	char *filename;
 	char **map;
 	int i;
@@ -37,24 +35,19 @@ int main(int argc, char *argv[])
 	(void)argc;
 	(void)argv;
 	filename = "map/map.ber";
-	//fd = open(filename, O_RDONLY);
 	map = read_map(filename);
 	if (!map)
 		return(0);
-
-	ft_printf("read ok\n");
 	if(!validate_map(map))
 	{
 		free_map(map);
 		return(0);
 	}
-	ft_printf("validace ok\n");
 	while (map[i])
 	{
 		ft_printf("%s", map[i]);
 		++i;
 	}
-	//close (fd);
 	free_map(map);
 	return(0);
 }
--- validation_map/validate_map.c
+++ validation_map/validate_map.c
@@ -6,7 +6,7 @@
 /*   By: haskalov <haskalov@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/10 13:37:42 by haskalov          #+#    #+#             */
-/*   Updated: 2026/02/16 22:25:38 by haskalov         ###   ########.fr       */
+/*   Updated: 2026/02/16 22:31:44 by haskalov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,33 +21,13 @@ int	right_map(char **map)
 	int	i;
 	int	len;
 	int	len2;
-	ft_printf("\n righ map start\n");
 
-
-
-if (!map)
-{
-    printf("map is NULL\n");
-    return 0;
-}
-if (!map[0])
-{
-    printf("first row is NULL\n");
-    return 0;
-}
-
-
-
-	
-	// if (!map || !map[0])
-   	// 	return (0);
+	if (!map || !map[0])
+   		return (0);
 	i = 1;
-	ft_printf(" map ok\n");
 	len = ft_strlen_map(map[0]);
 	if (!len)
 		return (0);
-
-	ft_printf("strlen ok\n");	
 	while (map[i] != NULL)
 	{
 		len2 = ft_strlen_map(map[i]);
@@ -97,11 +77,9 @@ int	walls_sides(char **map)
 	}
 	i = 0;
 	j = ft_strlen_map(map[1]) - 1;
-	j = 13;
 	ft_printf("%dj:\n",j );
 	while(map[i] != NULL)
 	{
-		ft_printf("\nposledni cislo: %c\n",(map[i][j]) );
 		if (map[i][j] != '1')
 			return(0);
 		++i;
@@ -111,14 +89,9 @@ int	walls_sides(char **map)
 
 int validate_map(char **map)
 {
-	
-	ft_printf("validate start");
 	if (!right_map(map))
 		return(0);
-	ft_printf("right map ok");
 	if (!walls_sides(map))
 		return(0);
-	ft_printf("walls sides ok");
 	return(1);
-
 }
